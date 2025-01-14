c2ba9309ef4dae40a518bd0e65cd2a4daed25c03
Louis Borroni <louis.borroni@yahoo.com>
2025-01-14 12:35:36 +0100

    registration back

---
+++ back/src/Controller/RegistrationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/api/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, UserRepository $userRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        // Check if email already exists
+        if ($userRepository->findOneBy(['email' => $data['email']])) {
+            return new JsonResponse(['error' => 'Email already used.'], JsonResponse::HTTP_CONFLICT);
+        }
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'User successfully registered'], JsonResponse::HTTP_CREATED);
+    }
+}
