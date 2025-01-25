343425ca4aa2891afc889ffc0a05383035ebc455
Louis Borroni <louis.borroni@yahoo.com>
2025-01-25 16:28:56 +0100

    homepage

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
 { path: '', redirectTo: '/login', pathMatch: 'full' },
